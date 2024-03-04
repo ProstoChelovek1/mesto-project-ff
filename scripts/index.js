d5bb009e145aa3dde4866515cbcccacd906e8d64
ProstoChelovek1 <105812350+ProstoChelovek1@users.noreply.github.com>
2024-03-04 19:28:26 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,84 +1,45 @@
 const content = document.querySelector('.content');
-const popupNew = document.querySelector('.popup_type_new-card')
 
 const placesContainer = content.querySelector('.places__list');
 const openButton = content.querySelector('.profile__add-button');
 
-const addButton = popupNew.querySelector('.popup__button');
-const closeButton = popupNew.querySelector('.popup__close');
-
 
 // @todo: Темплейт карточки
 
-openButton.addEventListener('click', openPopup);
-
-function openPopup() {
-    popupNew.classList.add('popup_is-opened');
-}
-
-closeButton.addEventListener('click', closePopup);
-
-function closePopup() {
-    popupNew.classList.toggle('popup_is-opened');
-}
 
 // @todo: DOM узлы
 
-function creatCard(placetValue, imageValue) {
-    const CardTemplate = document.querySelector('#card-template').content;
-    const CardElement = CardTemplate.querySelector('.card').cloneNode(true);
+function creatCard(initialCardsLocal, delelteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-    CardElement.querySelector('.card__title').textContent = placetValue;
-    CardElement.querySelector('.card__image').src = imageValue;
+    cardElement.querySelector('.card__title').textContent = initialCardsLocal.name;
+    cardElement.querySelector('.card__image').src = initialCardsLocal.link;
 
-
-    CardElement.querySelector('.card__like-button').addEventListener('click', function(evt) {
-        if (this.classList.contains('card__like-button_is-active')) {
-            evt.target.classList.remove('card__like-button_is-active');
-        } else {
-            evt.target.classList.add('card__like-button_is-active');
-        }
-    });
-
-    var delelteButton = CardElement.querySelector(".card__delete-button");
+    const delelteButton = cardElement.querySelector(".card__delete-button");
     delelteButton.addEventListener("click", function() {
-        delelteCard(CardElement);
+        delelteCard(cardElement);
     });
 
-    return CardElement;
+    return cardElement;
 }
 // @todo: Функция создания карточки
 
-function addCard(placetValue, imageValue) {
-    placesContainer.append(creatCard(placetValue, imageValue));
+function addCard(initialCardsLocal, delelteCard) {
+    placesContainer.append(creatCard(initialCardsLocal, delelteCard));
 }
 // @todo: Функция удаления карточки
 
-function delelteCard(CardElement) {
-    CardElement.remove();
-}
+const delelteCard = (cardElement) => {
+  cardElement.remove();
+};
 
 // @todo: Вывести карточки на страницу
 
-addButton.addEventListener('click', function() {
-    const place = popupNew.querySelector('.popup__input_type_card-name');
-    const image = popupNew.querySelector('.popup__input_type_url');
-
-    addCard(place.value, image.value);
-
-    const initialCardsAdd = {
-        name: place.value,
-        link: image.value
-    };
-    initialCards.push(initialCardsAdd);
-
-
-    place.value = '';
-    image.value = '';
-
-    closePopup();
-});
-
 for (let i = 0; i < initialCards.length; i++) {
-    addCard(initialCards[i].name, initialCards[i].link);
+    const initialCardsLocal = { 
+        name: initialCards[i].name, 
+        link: initialCards[i].link 
+    }; 
+    addCard(initialCardsLocal, delelteCard);
 }
